2a5786838d25917d297c5e4d8ae66e976d0146da
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-01-12 09:30:16 -0800

    almost  done with project

--- src/reducers/postsReducer.tsx
+++ src/reducers/postsReducer.tsx
@@ -1,20 +1,20 @@
 import * as actions from '../actions/postsActions'
 
-
 export const initialState = {
   posts: [],
   loading: false,
   hasErrors: false,
 }
 
-
-
-export default function postsReducer (state = initialState, action: { type: any }) {
+export default function postsReducer(state = initialState, action) {
   switch (action.type) {
+    case actions.GET_POSTS:
+      return { ...state, loading: true }
+    case actions.GET_POSTS_SUCCESS:
+      return { posts: action.payload, loading: false, hasErrors: false }
+    case actions.GET_POSTS_FAILURE:
+      return { ...state, loading: false, hasErrors: true }
     default:
       return state
   }
 }
-
-case actions.GET_POSTS:
-return { ...state, loading: true }
